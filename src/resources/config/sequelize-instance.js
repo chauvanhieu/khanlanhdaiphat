b313c818884bdd968f360c02ce557ad14c5cf424
chauvanhieu <chauhieu.nina@gmail.com>
2023-06-30 16:44:19 +0700

    upload

--- src/resources/config/sequelize-instance.js
+++ src/resources/config/sequelize-instance.js
@@ -3,19 +3,13 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize("khanlanhdaiphat", "root", "", {
   host: "localhost",
   dialect: "mysql",
+  logging: false,
 });
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Kết nối cơ sở dữ liệu thành công!");
-  })
-  .catch((error) => {
-    console.error("Lỗi kết nối cơ sở dữ liệu:", error);
-  });
-
-sequelize.sync().then(() => {
-  console.log("Đồng bộ hóa mô hình thành công!");
+sequelize.authenticate().catch((error) => {
+  console.error("Lỗi kết nối cơ sở dữ liệu:", error);
 });
+
+sequelize.sync();
 
 module.exports = sequelize;
