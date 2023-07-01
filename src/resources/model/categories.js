87d8ebae035d8e6c11f6f037032230803682413e
chauvanhieu <chauhieu.nina@gmail.com>
2023-07-01 15:52:00 +0700

    upload

--- src/resources/model/categories.js
+++ src/resources/model/categories.js
@@ -22,18 +22,6 @@ const Category = sequelize.define(
       type: DataTypes.INTEGER,
       allowNull: false,
     },
-    seo_title: {
-      type: DataTypes.STRING(300),
-      allowNull: false,
-    },
-    seo_keywords: {
-      type: DataTypes.STRING(500),
-      allowNull: false,
-    },
-    seo_description: {
-      type: DataTypes.STRING(600),
-      allowNull: false,
-    },
   },
   {
     tableName: "categories",
