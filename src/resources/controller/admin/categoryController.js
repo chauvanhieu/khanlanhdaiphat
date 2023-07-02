7e7a00c389c26b4751bc56c20020cb3a845ae13b
Uncle K in 2077 <vaiii147258@gmail.com>
2023-07-03 01:26:36 +0700

    admin Category CRUD function

--- src/resources/controller/admin/categoryController.js
+++ src/resources/controller/admin/categoryController.js
@@ -1,6 +1,20 @@
 const Category = require("../../model/categories");
 const Sequelize = require("sequelize");
 
+function removeAccent(str) {
+  str = str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  str = str.toLowerCase();
+  str = str.trim();
+  return str.replace(/[^a-z0-9]+/g, "-");
+}
+
+function generateSlug(title) {
+  // Loại bỏ các dấu tiếng Việt và chuyển đổi chuỗi thành slug
+  const slug = removeAccent(title);
+  return slug;
+}
+
+
 class AdminCategoryController {
   async index(req, res) {
     let listCategories = [];
@@ -16,7 +30,7 @@ class AdminCategoryController {
             name: {
               [Sequelize.Op.like]: `%${keyword}%`,
             },
-          }
+          },
         ],
       };
     }
@@ -29,7 +43,7 @@ class AdminCategoryController {
           id: item.id,
           name: item.name,
           slug: item.slug,
-          status: item.status
+          status: item.status,
         });
       });
     });
@@ -38,6 +52,66 @@ class AdminCategoryController {
       categories: listCategories,
       keyword: keyword,
     });
+  }
+
+  async create(req, res) {
+    const { newCategory } = req.body;
+    try {
+      const category = await Category.create({
+        name: newCategory,
+        slug: generateSlug(newCategory),
+        status: 1,
+      });
+      res.redirect("/admin/danh-muc");
+    } catch (error) {
+      // Xử lý lỗi nếu có
+      console.error(error);
+      res.render("adminTemplate/danh-muc", {
+        error: "Failed to create category",
+      });
+    }
+  }
+
+  async update(req, res) {
+    const { id } = req.body;
+    const { newCategoryName } = req.body;
+    const { newStatus } = req.body;
+
+    try {
+      const category = await Category.findByPk(id);
+      if (!category) {
+        return res.redirect("/admin/danh-muc");
+      }
+
+      category.name = newCategoryName;
+      category.slug = generateSlug(newCategoryName);
+      category.status = newStatus;
+
+      await category.save();
+
+      return res.redirect("/admin/danh-muc");
+    } catch (error) {
+      return res.status(500).json({ error: "Lỗi server" });
+    }
+  }
+
+  async updateStatus(req, res) {
+    const { id } = req.params;
+    const { newCategoryStatus } = req.body;
+
+    try {
+      const category = await Category.findByPk(id);
+      if (!category) {
+        return res.redirect("/admin/danh-muc");
+      }
+
+      category.status = newCategoryStatus;
+      await category.save();
+
+      return res.redirect("/admin/danh-muc");
+    } catch (error) {
+      return res.status(500).json({ error: "Lỗi server" });
+    }
   }
 }
 module.exports = new AdminCategoryController();
--- src/resources/router/adminRouter/category.js
+++ src/resources/router/adminRouter/category.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const adminCategoryController = require("../../controller/admin/categoryController");
 
 router.get("/", adminCategoryController.index);
+router.post("/", adminCategoryController.create);
+router.post("/update", adminCategoryController.update);
+router.post("/updateStatus", adminCategoryController.updateStatus);
 
 module.exports = router;
